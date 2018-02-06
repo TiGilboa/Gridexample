776f2ec311228ca6a2d21faa0b92e0930f107ea2
Tara Irene Gilboa <tara.gilboa@gmail.com>
2018-02-06 11:13:52 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("this is my javascript file");
+$("document").ready(function(){
+  console.log("thisisjqueryhappening");
+
+  $('#button').click(function(){
+    alert("get started!")
+  })
+
+
+
+})
